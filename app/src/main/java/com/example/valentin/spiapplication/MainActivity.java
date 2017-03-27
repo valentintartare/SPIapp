2de006699486054dedbc97533b00d728930e7096
Valentin Bias <Valentin Bias>
2017-03-27 23:32:31 +0200

    Last Commit

--- app/src/main/java/com/example/valentin/spiapplication/MainActivity.java
+++ app/src/main/java/com/example/valentin/spiapplication/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.valentin.spiapplication;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,36 +37,54 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     //Variable globales requise pour les sauvegardes BUNDLE
-    ArrayList<String> tabtaches=new ArrayList<String>();
-    Date date=new Date();
+    ArrayList<String> tabtaches = new ArrayList<String>();
+    Date date = new Date();
     String tache;
-    int id=0;
-    ArrayList<String> pieceajoute=new ArrayList<String>();
-    Date [] tempsTravail=new Date[2];
-    Boolean valide=new Boolean(false);
-    SetConnexionRest connexion=new SetConnexionRest();
+    int id = 0;
+    ArrayList<String> pieceajoute = new ArrayList<String>();
+    Date[] tempsTravail = new Date[2];
+    Boolean valide = new Boolean(false);
+
+    //Variables photo globales
+    SetConnexionRest connexion = new SetConnexionRest();
+    Button takePictureButton;
+    ImageView imageView;
+    Uri file;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        date.setYear(2000+Integer.parseInt((date.getYear()+"").substring(1,3)));
+        date.setYear(2000 + Integer.parseInt((date.getYear() + "").substring(1, 3)));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     public void login(View view) {
         EditText eT = (EditText) findViewById(R.id.emailChamp);
+        EditText pass = (EditText) findViewById(R.id.Password);
         final String login = eT.getText() + "";
+        final String password = pass.getText() + "";
+        /*TEMPORAIRE*/
+        if(login.equals("tech")){
+            afficheCalendar();
+        }else if(login.equals("client")){
+            id=5;
+            setContentView(R.layout.client_choix);
+        }
 
-
-        connexion.appelRestUser(login, this, new Response.Listener<String>() {
+        /*connexion.appelRestUser(login, password, this, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 response = response.substring(1);
@@ -55,9 +92,10 @@ public class MainActivity extends AppCompatActivity {
                 Gson test = new Gson();
                 test testage = test.fromJson(response, test.class);
                 if (testage.getName().compareTo(login) == 0) {
-                    afficheCalendar();
+                afficheCalendar();
                 }
             }
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -65,18 +103,18 @@ public class MainActivity extends AppCompatActivity {
                 erreur.setText("Echec de la connexion, vous n'êtez pas inscrit !\n\n - Si vous êtes un technicien, " +
                         "contactez un administrateur afin qu'un compte vous soit crée\n- Si vous êtes un client, inscrivez vous sur le site WEB et réessayez");
             }
-        });
+        });*/
     }
 
-    public void selectDate(View view){
-        id=2;
-        CalendarView cV=(CalendarView)findViewById(R.id.simpleCalendarView);
-        System.out.println("La date est :"+date);
+    public void selectDate(View view) {
+        id = 2;
+        CalendarView cV = (CalendarView) findViewById(R.id.simpleCalendarView);
+        System.out.println("La date est :" + date);
         setContentView(R.layout.tache_jour);
-        final ListView maisie=(ListView)findViewById(R.id.listItem);
-        TextView tV=(TextView)findViewById(R.id.datejour);
-        tV.setText("Taches du "+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getYear());
-        if(tabtaches.isEmpty()) {
+        final ListView maisie = (ListView) findViewById(R.id.listItem);
+        TextView tV = (TextView) findViewById(R.id.datejour);
+        tV.setText("Taches du " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getYear());
+        if (tabtaches.isEmpty()) {
             tabtaches.add("Roulettes : Auchan Leers");
             tabtaches.add("Ressort : E.Leclerc Tourcoing");
             tabtaches.add("Poulie : Amazon Paris");
@@ -85,48 +123,48 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, tabtaches);
         maisie.setAdapter(adapter);
-        maisie.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        maisie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                tache=maisie.getItemAtPosition(position).toString();
+                tache = maisie.getItemAtPosition(position).toString();
                 System.out.println(tache);
                 rapportGet();
             }
         });
     }
 
-    public void retourCalendar(View view){
-        if(!tabtaches.isEmpty()){
-           tabtaches.clear();
+    public void retourCalendar(View view) {
+        if (!tabtaches.isEmpty()) {
+            tabtaches.clear();
         }
         afficheCalendar();
     }
 
-    public void afficheCalendar(){
-        id=1;
+    public void afficheCalendar() {
+        id = 1;
         setContentView(R.layout.emploi_du_temps);
-        CalendarView cV=(CalendarView)findViewById(R.id.simpleCalendarView);
+        CalendarView cV = (CalendarView) findViewById(R.id.simpleCalendarView);
         cV.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                date=new Date(year,month,dayOfMonth);
+                date = new Date(year, month, dayOfMonth);
             }
         });
 
     }
 
     public void rapportGet() {
-        id=3;
+        id = 3;
         setContentView(R.layout.entrer_rapport);
-        Button bt=(Button)findViewById(R.id.depart);
+        Button bt = (Button) findViewById(R.id.depart);
         bt.setClickable(false);
-        TextView tV3=(TextView)findViewById(R.id.votrerapport);
-        tV3.setText("Rapport : "+tache);
-        Button button=(Button)findViewById(R.id.retourintervention);
-        final TextView tV4=(TextView)findViewById(R.id.pieceadd);
-        Button add=(Button)findViewById(R.id.addpiece);
-        final ListView listView=(ListView)findViewById(R.id.liste);
+        TextView tV3 = (TextView) findViewById(R.id.votrerapport);
+        tV3.setText("Rapport : " + tache);
+        Button button = (Button) findViewById(R.id.retourintervention);
+        final TextView tV4 = (TextView) findViewById(R.id.pieceadd);
+        Button add = (Button) findViewById(R.id.addpiece);
+        final ListView listView = (ListView) findViewById(R.id.liste);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -139,14 +177,14 @@ public class MainActivity extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pieceajoute.add(tV4.getText()+"");
+                pieceajoute.add(tV4.getText() + "");
                 tV4.setText("");
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
                         android.R.layout.simple_list_item_1, pieceajoute);
                 listView.setAdapter(adapter);
             }
         });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -159,93 +197,210 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void returntache(View view) {
-        id=2;
+        id = 2;
         CalendarView cV = (CalendarView) findViewById(R.id.simpleCalendarView);
         System.out.println("La date est :" + date);
         setContentView(R.layout.tache_jour);
         final ListView maisie = (ListView) findViewById(R.id.listItem);
         TextView tV = (TextView) findViewById(R.id.datejour);
-        if(tabtaches.isEmpty()){
-            TextView end=(TextView)findViewById(R.id.listeVide);
+        if (tabtaches.isEmpty()) {
+            TextView end = (TextView) findViewById(R.id.listeVide);
             end.setText("Vous avez completer toutes vos taches");
         }
-        tV.setText("Taches du " + date.getDate() + "/" + (date.getMonth()+1) + "/" +date.getYear());
+        tV.setText("Taches du " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getYear());
         ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, tabtaches);
         maisie.setAdapter(adapter);
         maisie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                tache=maisie.getItemAtPosition(position).toString();
+                tache = maisie.getItemAtPosition(position).toString();
                 System.out.println(tache);
                 rapportGet();
             }
         });
     }
 
-    public void onSaveInstanceState(Bundle outState)
-    {
-        outState.putInt("id",id);
-        outState.putStringArrayList("listetache",tabtaches);
-        if(date!=null) {
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("id", id);
+        outState.putStringArrayList("listetache", tabtaches);
+        if (date != null) {
             outState.putInt("jour", date.getDate());
             outState.putInt("month", (date.getMonth()));
             outState.putInt("year", date.getYear());
         }
-        if(tache!=null){
-            outState.putString("tache",tache);
+        if (tache != null) {
+            outState.putString("tache", tache);
         }
     }
 
-    public void onRestoreInstanceState(Bundle savedInstanceState){
-        id=savedInstanceState.getInt("id");
-        if(id==1){
-           afficheCalendar();
-        } else if(id>1){
-            date=new Date(savedInstanceState.getInt("year"),savedInstanceState.getInt("month"),savedInstanceState.getInt("jour"));
-            tabtaches=savedInstanceState.getStringArrayList("listetache");
-            tache=savedInstanceState.getString("tache");
-            if(id==2)
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        id = savedInstanceState.getInt("id");
+        if (id == 1) {
+            afficheCalendar();
+        } else if (id > 1 && id<4) {
+            date = new Date(savedInstanceState.getInt("year"), savedInstanceState.getInt("month"), savedInstanceState.getInt("jour"));
+            tabtaches = savedInstanceState.getStringArrayList("listetache");
+            tache = savedInstanceState.getString("tache");
+            if (id == 2)
                 selectDate(new View(MainActivity.this));
             else
                 rapportGet();
+        }else if(id==5){
+            retourMenuClient(new View(MainActivity.this));
+        }else if(id==6){
+            prendrePhoto(new View(MainActivity.this));
+        }else if(id==7){
+            goMaintenance(new View(MainActivity.this));
         }
 
     }
 
-    public String getHeure(boolean time){
+    public String getHeure(boolean time) {
         Date d = new Date();
-        if(time)
-            tempsTravail[0]=d;
+        if (time)
+            tempsTravail[0] = d;
         else
-            tempsTravail[1]=d;
+            tempsTravail[1] = d;
         SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
         String s = f.format(d);
-        s=s.substring(9,11)+"H"+s.substring(11,13);
+        s = s.substring(9, 11) + "H" + s.substring(11, 13);
         return s;
     }
-    public void setHeureArrivee(View view){
-        TextView tV=(TextView)findViewById(R.id.setArrival);
-        Button btdep=(Button)findViewById(R.id.depart);
-        Button bt=(Button)findViewById(R.id.ButArrive);
-        tV.setText("Arrivée : "+getHeure(true));
+
+    public void setHeureArrivee(View view) {
+        TextView tV = (TextView) findViewById(R.id.setArrival);
+        Button btdep = (Button) findViewById(R.id.depart);
+        Button bt = (Button) findViewById(R.id.ButArrive);
+        tV.setText("Arrivée : " + getHeure(true));
         btdep.setClickable(true);
         bt.setClickable(false);
     }
 
-    public void setHeureDepart(View view){
-        TextView tV=(TextView)findViewById(R.id.setDeparture);
-        tV.setText("Départ : "+getHeure(false));
+    public void setHeureDepart(View view) {
+        TextView tV = (TextView) findViewById(R.id.setDeparture);
+        tV.setText("Départ : " + getHeure(false));
         setTempsTravail();
     }
 
-    public void setTempsTravail(){
-        TextView tV=(TextView)findViewById(R.id.tdtotal);
-        Date tdt=new Date((tempsTravail[1].getTime()-tempsTravail[0].getTime())-3600000);
+    public void setTempsTravail() {
+        TextView tV = (TextView) findViewById(R.id.tdtotal);
+        Date tdt = new Date((tempsTravail[1].getTime() - tempsTravail[0].getTime()) - 3600000);
         SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
         String s = f.format(tdt);
-        s=s.substring(9,11)+"H"+s.substring(11,13);
-        tV.setText("Temps de travail total : "+s);
+        s = s.substring(9, 11) + "H" + s.substring(11, 13);
+        tV.setText("Temps de travail total : " + s);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                takePictureButton.setEnabled(true);
+            }
+        }
+    }
+
+    public void prendrePhoto(View view){
+        id=6;
+        setContentView(R.layout.prise_photo);
+        Button rapport=(Button) findViewById(R.id.suiteSAV);
+        rapport.setVisibility(View.INVISIBLE);
+    }
+
+    public void takePicture(View view) {
+        takePictureButton = (Button) findViewById(R.id.button_image);
+        Button rapport=(Button) findViewById(R.id.suiteSAV);
+        rapport.setVisibility(View.VISIBLE);
+        imageView= (ImageView) findViewById(R.id.imageview);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(file);
+            }
+        }
+    }
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+    public void retourMenuClient(View view){
+        id=5;
+        setContentView(R.layout.client_choix);
+    }
+
+    public void goMaintenance(View view){
+        id=7;
+        String [] tab=new String[]{"Nettoyage","Révision","Réparation"};
+        setContentView(R.layout.rdv_maintenance);
+        final ListView lV=(ListView)findViewById(R.id.listIntervention);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_checked, tab);
+        lV.setAdapter(adapter);
+        lV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                lV.setItemChecked(position,true);
+            }
+        });
+    }
+
+    public void mail(View view){
+        setContentView(R.layout.webnav);
+        Button maileur=(Button)findViewById(R.id.mail);
+        maileur.setVisibility(View.INVISIBLE);
+        Button web=(Button)findViewById(R.id.backSite);
+        web.setVisibility(View.VISIBLE);
+        connexionWeb("https://www.google.com/gmail/");
+    }
+
+    public void goOnSite(View view){
+        setContentView(R.layout.webnav);
+        Button web=(Button)findViewById(R.id.backSite);
+        web.setVisibility(View.INVISIBLE);
+        Button maileur=(Button)findViewById(R.id.mail);
+        maileur.setVisibility(View.VISIBLE);
+        connexionWeb("https://www.google.com");
+
+    }
+
+    public void connexionWeb(String url){
+        WebView webView=(WebView)findViewById(R.id.navsite);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(url);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
     }
 
 }
--- app/src/main/java/com/example/valentin/spiapplication/SetConnexionRest.java
+++ app/src/main/java/com/example/valentin/spiapplication/SetConnexionRest.java
@@ -3,6 +3,7 @@ package com.example.valentin.spiapplication;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -11,19 +12,29 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Valentin on 27/03/2017.
  */
 
 public class SetConnexionRest {
     boolean connexion=false;
-    volatile String message;
+    String message;
 
-    synchronized public void appelRestUser(final String login,final MainActivity main, Response.Listener<String> listener, Response.ErrorListener errorListener){
+    public void appelRestUser(final String login, final String mdp,final MainActivity main, Response.Listener<String> listener, Response.ErrorListener errorListener){
         RequestQueue queue = Volley.newRequestQueue(main);
-        String url="http://51.255.131.194/v1/user";
+        String url="http://51.255.131.194/secure/who";
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,listener, errorListener);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,listener,errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", "Basic "+login+":"+mdp);
+                return params;
+            }
+        };
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
